7430efe196eb6eaf47054bda7c489a7c8857c563
gmallard <allard.guy.m@gmail.com>
2016-08-29 18:06:02 -0400

    Run jruby unit tests more easily.

--- test/unitst.sh
+++ test/unitst.sh
@@ -6,11 +6,12 @@ cmd_base=$(dirname $0)
 source $cmd_base/funcs.sh
 # ------------------------------------------------------------------------------
 pushd $cmd_base
+RUN=${RUBY:-ruby}
 slist=$(ls -1 *.rb | grep -v helper | grep -v 1method)
 for rbf in $slist
 do
-	echo "ruby -I ../lib ${rbf}"
-	ruby -I ../lib $rbf
+	echo "${RUN} -I ../lib ${rbf}"
+	$RUN -I ../lib $rbf
 done
 # ------------------------------------------------------------------------------
 popd
