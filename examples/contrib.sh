ed58f44a4074e7bc392a11a0c9aa613700e5bcf3
Guy M. Allard <allard.guy.m@gmail.com>
2017-03-02 08:57:00 -0500

    Modify example contributors.rb for more general use.

--- examples/contrib.sh
+++ examples/contrib.sh
@@ -2,5 +2,5 @@
 #
 # git log --reverse --all --date=short --pretty --format='%aI;%cI;%cn;%ce' | \
 git log --reverse --all --date=short --pretty --format='%ad;%cd;%cn;%ce' | \
-    ruby examples/contributors.rb
+    ruby $HOME/bin/contributors.rb
  
