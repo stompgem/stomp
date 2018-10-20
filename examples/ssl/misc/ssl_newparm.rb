432f0e1c305fbdf07f94044623548d289545db7f
Guy M. Allard <allard.guy.m@gmail.com>
2018-10-20 10:59:39 -0400

    Refactor use of SSL context parameters.

--- examples/ssl/misc/ssl_newparm.rb
+++ examples/ssl/misc/ssl_newparm.rb
@@ -3,8 +3,16 @@
 #
 # Reference: https://github.com/stompgem/stomp/wiki/extended-ssl-overview
 #
-require "rubygems"
-require "stomp"
+if Kernel.respond_to?(:require_relative)
+  require_relative("../ssl_common")
+  require_relative("../../stomp_common")
+else
+  $LOAD_PATH << File.dirname(__FILE__)
+  require "../ssl_common"
+  require "../../stomp_common"
+end
+include SSLCommon
+include Stomp1xCommon
 #
 # == Demo override of SSLContext.new parameters.
 #
@@ -14,30 +22,32 @@ require "stomp"
 class ExampleSSLNewParm
   # Initialize.
   def initialize
+		@port = ENV['STOMP_PORT'] ? ENV['STOMP_PORT'].to_i : 61611
   end
   # Run example.
   def run
-    ssl_opts = Stomp::SSLParams.new
+    ssl_opts = Stomp::SSLParams.new()
     hash = { :hosts => [
-        {:login => 'guest', :passcode => 'guest', :host => 'localhost', :port => 61612, :ssl => ssl_opts},
+        {:login => login(), :passcode => passcode(), :host => host(), :port => @port, :ssl => ssl_opts},
       ],
-      :reliable => false, # YMMV, to test this in a sane manner
-      :sslctx_newparm => :SSLv2,  # An example should you:
-      # a) Actually want SSLv2 *AND*
-      # b) Your Ruby version supports SSLv2 *AND*
-      # c) Your broker supports and allows SSLv2
+      :reliable => false,         # YMMV, to test this in a sane manner
+      # This parameter is passed by the gem to the new method for ssl context.
+      :sslctx_newparm => :TLSv1,  # An example, try to force TLSv1
+      # If your version of Ruby does not support what you ask for, Ruby openssl code will raise
+      # an exception.  If Ruby accepts your parms, but your broker does not you will get
+      # different errors (depending on broker).
     }
     #
-    puts "Connect starts, SSL Use Case 1"
+    puts "Connect starts, Demo SSL context new parameters"
     c = Stomp::Connection.new(hash)
     puts "Connect completed"
     puts "SSL Verify Result: #{ssl_opts.verify_result}"
-    # puts "SSL Peer Certificate:\n#{ssl_opts.peer_cert}"
+    puts "SSL Peer Certificate:\n#{ssl_opts.peer_cert}" if showPeerCert()
     #
-    c.disconnect
+    c.disconnect()
   end
 end
 #
-e = ExampleSSLNewParm.new
-e.run
+e = ExampleSSLNewParm.new()
+e.run()
 
