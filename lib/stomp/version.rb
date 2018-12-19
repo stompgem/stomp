b064acff4150b37fce2b85ce327f84765749a10e
Guy M. Allard <allard.guy.m@gmail.com>
2018-12-19 10:50:13 -0500

    Start next release series.

--- lib/stomp/version.rb
+++ lib/stomp/version.rb
@@ -6,8 +6,8 @@ module Stomp
   module Version  #:nodoc: all
     MAJOR = 1
     MINOR = 4
-    PATCH = 8
-    # PATCH = "8.plvl.001"
+    # PATCH = 8
+    PATCH = "8.plvl.001"
     STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
   end
 end
